d047b00f9139de0cabd56061f99c0782d6a3bdc9
Mwangi <mwangi@localhost.localdomain>
2021-04-20 22:20:43 -0400

    file structure and tailwind setup

---
+++ src/components/home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <div class="justify center text-center ">
+                <p class="">
+                Welcome page. Welcome to thrift forum this is a community for sharing ideas, stories, memes, Thrift_findings, thrift_advice.
+                </p>
+                <p class="">In order to make the community worthwhile and valuable to everyone, there needs to be rules. We also need your help in making the community great. By spamming posts that are unrelated to the</p>
+                <p class="">Rules for this community include:
+                <ul>1. No advertising  or any sort of self promotion Any bit of advertising the post will be deleted and the user gets banned.</ul>
+                 <ul>2. No posts unrelated to what the forum is about. (thrift or fashion related content</ul>
+                <ul>3. No adult content related posts</ul>
+                </p>
+                <p class="">Any single person who might go against these rules will be permanently or temporarily be banned from the community.</p>
+                <p>You might find yourself not able to use certain categories. This is because you need to earn points to be trusted enough to make valuable posts to the thrift community. In order to get access to them you need to share valuable posts or likeable posts earning points and getting access to categories such as #advice.</p>
+                <button class="btn btn-blue"> I understand</button>
+            </div>            
+        </div>
+    )
+}
